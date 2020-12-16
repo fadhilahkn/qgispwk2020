6f5b26a09154b4a9bc42f586392a15a0105ab49b
fadhilahkn <76077316+fadhilahkn@users.noreply.github.com>
2020-12-16 07:05:59 +0700

    Add files via upload

---
+++ data/acuankopi_1.js
@@ -0,0 +1,8 @@
+var json_acuankopi_1 = {
+"type": "FeatureCollection",
+"name": "acuankopi_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "Point", "coordinates": [ 109.131449578813402, -6.869731973863781 ] } }
+]
+}
